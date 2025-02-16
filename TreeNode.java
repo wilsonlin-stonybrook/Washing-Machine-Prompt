fe13a638992c2ea683076401943390c0ea8ea6f7
wilsonlin-stonybrook <wilson.lin.2@stonybrook.edu>
2025-02-16 18:09:32 -0500

    Update TreeNode.java

--- TreeNode.java
+++ TreeNode.java
@@ -1,10 +1,3 @@
-/**
- * Wilson Lin
- * 115091711
- * wilson.lin.2@stonybrook.edu
- * Hw 4
- * CSE214.R04 Summer 2024
- */
 
 /**
  * The <code>TreeNode</code> class is used for the creation of a tree node object
